af50f8822dccaae30118019ed5b7464c1421006a
dD <martin.cavazos.alvarado@gmail.com>
2018-07-23 14:18:22 -0500

    Scrip pls
    ahhhhhhhhhhhhhh

---
+++ escenarioJAP/Assets/SimpleController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleController : MonoBehaviour 
+{
+	private CharacterController controller;
+	
+	public float speed;
+	public float jumpSpeed;
+	
+	public Animator animator;
+	private Vector3 moveDirection;
+	public float gravity = 50;
+	
+	public float rotationSpeed = 150;
+	
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update () 
+	{
+		
+	
+		if (controller.isGrounded) 
+		{
+			transform.Rotate(Vector3.up*rotationSpeed*Input.GetAxis("Horizontal")*Time.deltaTime);
+			moveDirection = new Vector3(0/*Input.GetAxis("Horizontal")*/,0,Input.GetAxis("Vertical"));		
+			if(moveDirection.magnitude>1)
+			{
+				moveDirection.Normalize();
+			}
+			animator.SetFloat("Velocidad", moveDirection.magnitude);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButtonDown("Jump"))
+			{
+				moveDirection.y = jumpSpeed;
+				animator.SetTrigger("Brinco");
+			}
+			else
+			{
+				if(Input.GetKeyDown(KeyCode.Q))
+				{
+					animator.SetTrigger("Ataque");
+				}
+			}
+            
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+		
+	}
+	
+}
+
+
+
+
+
+
+
---
+++ escenarioJAP/Assets/SimpleMatchRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMatchRotation : MonoBehaviour
+{
+	public float speedRotation,speedPosition;
+	public Transform target;
+
+	
+	void LateUpdate () 
+	{
+		transform.position = Vector3.Lerp(transform.position,target.position, Time.deltaTime * speedPosition );
+		transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.deltaTime* speedRotation);
+		
+	}
+}
